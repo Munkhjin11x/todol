c28923a49250710d690182d3a4614084cc304fb4
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2023-11-06 01:24:39 +0800

    snu

--- index.js
+++ index.js
@@ -12,15 +12,19 @@ let progress = [
       title: "done",
     },
   ];
-  function createcards(progress) {
-    let { title } = progress;
-    return `<div>
-    <h1>${title}</h1>
-     <div class="card"></div>
-    <button id ="addcard">+addcard</button> 
-    </div>`;
-  }
+  let data = [
+    {
+      title: "Todo",
+      desc: "123",
+      status: "inprogress"
+    },
+    {
+      title: "todo",
+      desc: "123",
+      status: "todo"
   
+    },
+  ];
   function listItem(props) {
     let { title, desc } = props;
     return `<div>
@@ -31,78 +35,39 @@ let progress = [
   }
   
   let body = document.querySelector("body");
-  let container = document.querySelector(".container");
-  let cards = document.querySelector(".cards");
-  let openModal = document.querySelector('.openContainer');
-  let addCardOpen = document.getElementById("addcardsnu");
-  let inputTitle = document.getElementById('InpTitle')
+  let openModal = document.querySelector('.modal');
+  let addCardOpen = document.querySelectorAll("#addcard");
+  let closeBtn =document.getElementById('closeBtn');
+  let card = document.querySelectorAll(".card");
+      
+  for (const card of addCardOpen ){
+    card.addEventListener('click', ()=>
+    openModalTrigger() 
+    )  
+  }
 
-  console.log(addCardOpen);
-  function openmodal() {
-    return `
-    <div class="openmodal">
-    <h2>Add task</h2>
-    <h3>Title</h3>
-    <input type="" id = "InpTitle">
-    <h3>description</h3>
-    <input type="" id="InpDesc">
-    <h3>status</h3>
-    <select id="status">
-  <option value="todo">todo</option>
-  <option value="inprogress">inprogress</option>
-  <option value="stuck">stuck</option>
-  <option value="done">done</option>
-     </select>
-     <p>priority</p>
-     <select id="priority">
-     <option value="high">high</option>
-     <option value="low">low</option>
-        </select>
-        <button id ='closeBtn'>Addcard</button>
-    </div>`
-   
+  function addTask() {
+    let title = document.getElementById('title').value;
+    let desc = document.getElementById('desc').value;
+    let status = document.getElementById('status').value;
+    data.push({title,desc,status})
+    render()
+    closeModalTrigger();
   }  
-  let data = [
-    {
-      title: "Todo",
-      desc: "123",
-      status: "done"
-    },
-    {
-      title: "Todo",
-      desc: "123",
-      status: "inprogress"
-    },
-    {
-      title: "Todo",
-      desc: "123",
-      status: "stuck"
-    },
-    {
-      title: "todo",
-      desc: "123",
-      status: "todo"
-  
-    },
-  ];
-  progress.map((a) => {
-    cards.innerHTML += createcards(a);
-  });
-
-  
+  closeBtn.onclick = addTask;
+  function render() {
+    card.forEach(card =>  card.innerHTML = "");
+ 
   data.map((e) => {
-    let card = document.querySelectorAll('.card');
-    if(e.status === "todo") {
-      card[0].innerHTML += listItem(e);
-    } else if(e.status === "inprogress"){
-      card[1].innerHTML += listItem(e);
-    }
-    else if(e.status === "stuck"){
-      card[2].innerHTML += listItem(e);
-    }
-    else if(e.status === "done"){
-      card[3].innerHTML += listItem(e);
-    }
-  });
-  openModal.innerHTML += openmodal()
-  addCardOpen.addEventListener("click", () => (openModal.style.display ='flex'))
+  if (e.status === 'todo') card[0].innerHTML += listItem(e);
+  if (e.status === 'inprogress') card[1].innerHTML += listItem(e);
+  if (e.status === 'stuck') card[2].innerHTML += listItem(e);
+  if (e.status === 'done') card[3].innerHTML += listItem(e);}
+  )};
+  render()
+  function openModalTrigger() {
+    openModal.style.display ='flex'
+  };
+  function closeModalTrigger() {
+    openModal.style.display ='none'
+  };
